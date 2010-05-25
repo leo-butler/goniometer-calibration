97f0a35d634b80e4fbdf9d2e295a58f90ce7f2bf
lbutler <devnull@localhost>
2010-05-25 20:40:48 +0100

    changed to use varargin

--- ifelse.m
+++ ifelse.m
@@ -1,18 +1,25 @@
-function b = ifelse (P)
-  ## usage:  b = ifelse (P)
+function b = ifelse (varargin)
+  ## usage:  b = ifelse (varargin)
   ##
-  ## P = [Prop1,Consequence1,...,PropN,ConsequenceN,DefaultConsequence]
+  ## varargin = Prop1,Consequence1,...,PropN,ConsequenceN,DefaultConsequence
   ## We evaluate each proposition in turn, until a true (!=0) one is found
   ## at which point the consequence is returned. If unspecified, the
   ## DefaultConsequence ('else') is 0.
-  lenp=length(P);
+  #varargin
+  lenp=length(varargin);
   if lenp==1
-    b=P(1);
-  elseif P(1)
-    b=P(2);
+    b=varargin{1};
+  elseif varargin{1}
+    b=varargin{2};
   elseif lenp>2
-    b=ifelse(P(3:lenp));
+    b=ifelse(varargin{3:lenp});
   else
     b=0;
   endif
 endfunction
+%!test
+%! assert(ifelse(1,2),2)
+%! assert(ifelse(1,2,3),2)
+%! assert(ifelse(0,2,3),3)
+%! assert(ifelse(0,2,0,3,1,4),4)
+%! assert(ifelse(0,2,0,3,1),1)
