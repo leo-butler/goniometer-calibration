bbc0151cf2705a3afad47b18ba0521d0a81e6e99
lbutler <devnull@localhost>
2010-05-27 10:03:06 +0100

    initial commit

---
+++ line_plot.m
@@ -0,0 +1,43 @@
+## -*- Mode: octave; Package: OCTAVE -*-
+##
+## $Id$
+##
+## Author: Leo Butler (l.butler@ed.ac.uk)
+##
+## This file is OCTAVE code (http://www.octave.org/)
+##
+## It is free software; you can redistribute it and/or modify
+## it under the terms of the GNU General Public License as published by
+## the Free Software Foundation; either version 3 of the License, or (at your
+## option) any later version.
+##
+## This software is distributed in the hope that it will be useful, but
+## WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
+## or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public
+## License for more details.
+##
+## You should have received a copy of the GNU General Public License
+## along with this file. If not, see http://www.gnu.org/licenses/.
+##
+
+source line.m
+
+function t = line_plot ()
+  ## usage:  t = line_plot ()
+  ##
+  ## 
+  global objectivefn_data objectivefn_partition;
+  symbols='+*ox^';
+  len=objectivefn_partition(1,:);
+  b=1;
+  f=0;
+  hold off;
+  for i=1:length(len)
+    f=i;
+    plot3(objectivefn_data(b:f,1),objectivefn_data(b:f,2),objectivefn_data(b:f,3),symbols(i));
+    hold on;
+    b=f;
+  endfor
+#  hold off;
+  t=1;
+endfunction
