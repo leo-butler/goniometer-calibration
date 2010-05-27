13f19892b0939d68c745cf3998bd1bc4297200d9
lbutler <devnull@localhost>
2010-05-27 23:09:29 +0100

    added head-matter

--- ifelse.m
+++ ifelse.m
@@ -1,3 +1,25 @@
+## -*- Mode: octave; Package: OCTAVE -*-
+##
+## $Id$
+##
+## Author: Leo Butler (l.butler@ed.ac.uk)
+##
+## This file is OCTAVE code (http://www.octave.org/)
+##
+## It is free software; you can redistribute it and/or modify
+## it under the terms of the GNU General Public License as published by
+## the Free Software Foundation; either version 3 of the License, or (at your
+## option) any later version.
+##
+## This software is distributed in the hope that it will be useful, but
+## WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
+## or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public
+## License for more details.
+##
+## You should have received a copy of the GNU General Public License
+## along with this file. If not, see http://www.gnu.org/licenses/.
+##
+
 function b = ifelse (varargin)
   ## usage:  b = ifelse (varargin)
   ##
@@ -25,4 +47,3 @@ endfunction
 %! assert(ifelse(0,2,0,3,1),1)
 
 ## end of ifelse.m
-
