3d58e9b1cfaa0ba5e46ececa0886af65e01a3345
lbutler <devnull@localhost>
2010-05-29 14:02:09 +0100

    extensive renovation, added a function interface to do CLS

--- goniometer.m
+++ goniometer.m
@@ -35,15 +35,84 @@ function [y,z] = gpartition (x)
   endfor
 endfunction
 
-niter=25;
-epsilon=1e-8;
 
-goniometer_file="goniometer.dat";
-goniometer_data=read_goniometer_data(goniometer_file)
-[objectivefn_data,objectivefn_partition]=gpartition(goniometer_data)
-make_objectivefn_lines();
-L0=reshape(objectivefn_lines(1:2,:)',6,1);
-[L,obj,info,iter,nf,lambda]=line_estimator(L0,[],[],niter,epsilon)
+function [L,obj,info,iter,nf,lambda,cons] = goniometer (filename,niter=45,epsilon=1e-8)
+  ## usage:  goniometer ()
+  ##
+  ## goniometer_file="goniometer.dat";
+  global objectivefn_data objectivefn_lines objectivefn_partition;
+  goniometer_data=read_goniometer_data(filename)
+  [objectivefn_data,objectivefn_partition]=gpartition(goniometer_data);
+  goniometer_rebase_zdata()
+  make_objectivefn_lines();
+  L0=reshape(objectivefn_lines(1:2,:)',6,1)
+  L0=L0 + 1e-2*norm(L0)*randn(6,1)
+  [L,obj,info,iter,nf,lambda]=line_estimator(L0,[],[],niter,epsilon)
+  cons=constraintfn(L)
+endfunction
+## from Octave manual page on `sqp':
+%!test
+%! epsilon=1e-7;
+%! x_e   = [-1.717143501952599;
+%!           1.595709610928535;
+%!           1.827245880097156;
+%!          -0.763643103133572;
+%!          -0.763643068453300];
+%!
+%! obj_e = 0.0539498477702739;
+%! info_e=101;
+%! iter_e=8;
+%! nf_e=10;
+%! lambda_e=[-0.0401627;0.0379578;-0.0052227];
+%! g=@(x) [ sumsq(x)-10;x(2)*x(3)-5*x(4)*x(5);x(1)^3+x(2)^3+1 ];
+%! phi=@(x) exp(prod(x)) - 0.5*(x(1)^3+x(2)^3+1)^2;
+%! x0=[-1.8; 1.7; 1.9; -0.8; -0.8];
+%! [x, obj, info, iter, nf, lambda] = sqp (x0, phi, g, []);
+%! assert(norm(x - x_e),0,epsilon)
+%! assert(norm(obj - obj_e),0,epsilon)
+## bugs in sqp.m:
+%! assert((info==info_e || info==0),1==1)
+%! assert((iter==iter_e || iter==iter_e+1),1==1)
+%! assert((nf==nf_e || nf==nf_e-1),1==1)
+%! assert(norm(lambda - lambda_e),0,epsilon)
+####from /usr/local/share/octave/3.2.4/m/optimization/sqp.m
+%!function r = g (x)
+%!  r = [sumsq(x)-10;
+%!       x(2)*x(3)-5*x(4)*x(5);
+%!       x(1)^3+x(2)^3+1 ];
+%!
+%!function obj = phi (x)
+%!  obj = exp(prod(x)) - 0.5*(x(1)^3+x(2)^3+1)^2;
+%!
+%!test
+%! x0 = [-1.8; 1.7; 1.9; -0.8; -0.8];
+%!
+%! [x, obj, info, iter, nf, lambda] = sqp (x0, @phi, @g, []);
+%!
+%! x_opt = [-1.717143501952599;
+%!           1.595709610928535;
+%!           1.827245880097156;
+%!          -0.763643103133572;
+%!          -0.763643068453300];
+%!
+%! obj_opt = 0.0539498477702739;
+%!
+%! assert (all (abs (x-x_opt) < 5*sqrt (eps)) && abs (obj-obj_opt) < sqrt (eps));
 
+function goniometer_rebase_zdata ()
+  ## usage:  goniometer_rebase_zdata ()
+  ##
+  ## 
+  global objectivefn_data;
+  r=rows(objectivefn_data);
+  c=columns(objectivefn_data);
+  for i=1:r
+    for j=1:c
+      x=objectivefn_data(i,j);
+      x=ifelse(x>1e6,mod(x,10000),x) * 1e-3;
+      objectivefn_data(i,j)=x;
+    endfor
+  endfor
+endfunction
 
 ## end of goniometer.m
