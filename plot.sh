694e26250335c82f3dbfb4db4061919832772bbd
reltub.oel <devnull@localhost>
2010-06-16 02:27:19 +0100

    Removed u2.m un_polar.m movie.m safe_strcat.m plot.sh ssum.m

--- plot.sh
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/octave
-x=0:0.1:2*pi;
-plot(2*sin(x));
-plot3(sin(x), cos(x), x);
--- safe_strcat.m
+++
@@ -1,6 +0,0 @@
-function s=safe_strcat(a,b)
-  s = deblank(strcat(deblank(a),deblank(b)));
-endfunction
-
-## end of safe_strcat.m
-
