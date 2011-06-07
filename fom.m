d16933abea705942d7654774110b9debcfdfb3fb
work <work@squeeeze.butler.org>
2011-06-06 20:41:04 -0400

    interim commit

--- fom.m
+++ fom.m
@@ -25,10 +25,12 @@ source filenames.m
 
 global plane_data_directory;
 plane_data_directory="~/svn-ecdf/goniometer-calibration/dir+/";
+global planes_objectivefn_data planes_objectivefn_partition planes_objectivefn_weights planes_objectivefn_scores;
+planes_objectivefn_weights=[1e-10;1;1e-10];
+planes_objectivefn_scores=[];
 
-global planes_objectivefn_data planes_objectivefn_partition;
-function t = fom (X,W=[1,1,1])
-  ## usage:  t = fom (X,W=[1,1,1])
+function t = fom (X)
+  ## usage:  t = fom (X)
   ##
   ## X = [L;PP;LL] where
   ##  L = 6x1 column vector (a line)
@@ -43,29 +45,25 @@ function t = fom (X,W=[1,1,1])
   ## * the first element in planes_objectivefn_data is the plane at 0.
   ##
   global planes_objectivefn_data planes_objectivefn_partition;
-  n=length(planes_objectivefn_partition);
-  pwl=find(planes_objectivefn_partition>1)
-  pwol=setdiff(1:n,pwl);
-  m=sum(planes_objectivefn_partition(pwl));
-  k=4*n+6*m+6;
-  l=length(X);
-  if l!=k
-    error ("fom(X): input X should have length ",k);
-  endif
-  pp=4*n+6;
-  L=X(1:6);
-  PP=reshape(X(7:pp),4,n);
-  LL=reshape(X(pp+1:l),6,m);
-  S=powersets(PP',[columns(PP);2])
+  global planes_objectivefn_weights planes_objectivefn_scores;
+  [L,PP,LL,S,pwl,pwol] = extract_components (X);
+  W=planes_objectivefn_weights;
+  scores=zeros(1,3);
 
   ## compute penalty for lines
   t=0;
   c=0;
   for i=pwl'
+    a=planes_objectivefn_data{i}.scalars(1);
+    v=planes_objectivefn_data{i}.dirvec;
+    w=planes_objectivefn_data{i}.tdirvec;
+    n=planes_objectivefn_data{i}.normal;
+    p=a*n;
     for j=1:length(planes_objectivefn_data{i}.lines)
       ++c;
       s=0;
-      M=LL(:,c);
+      b=planes_objectivefn_data{i}.scalars(j+1);
+      M=[p+b*w;v];
       for x=(planes_objectivefn_data{i}.lines{j})'
 	s+=dpoint2line(x',M)^2;
       endfor
@@ -75,18 +73,20 @@ function t = fom (X,W=[1,1,1])
       t+=s;
     endfor
   endfor
+  scores(1)=t;
 
   ## compute penalty for planes
   s=0;
-  L=reshape(L,3,2)'
+  L=reshape(L,3,2)';
   for p=(S.partition){1}'
-    A=PP(:,p(1))
-    B=PP(:,p(2))
-    M=intersection_line(A,B)
+    A=PP(:,p(1));
+    B=PP(:,p(2));
+    M=intersection_line(A,B);
     s+=line_obj(L,M);
   endfor
   s*=W(2);
   t+=s;
+  scores(2)=s;
 
   ## finally, compute the penalty for the 0 inclination plane
   s=0;
@@ -97,17 +97,154 @@ function t = fom (X,W=[1,1,1])
     endfor
     N=length(planes_objectivefn_data{i}.lines{1});
     s/=N;
+    s*=W(3);
     t+=s;
   endfor
-
+  scores(3)=s;
+  planes_objectivefn_scores=[planes_objectivefn_scores;scores];
 endfunction
+
 %!test
 %! global planes_data_directory planes_objectivefn_partition;
 %! planes_data_directory="~/svn-ecdf/goniometer-calibration/dir+/";
 %! planes=get_planes(planes_data_directory);
-%! l=6+4*length(planes_objectivefn_partition)+6*sum(planes_objectivefn_partition(find(planes_objectivefn_partition>1)));
-%! X=1:l;
+%! l=6+4*1+6*2+9+2;
+%! X=randn(l,1);
+%! tic
 %! fom(X)
+%! toc
+
+function [L,PP,LL,S,pwl,pwol] = extract_components (X)
+  ## usage:  extract_components (X)
+  ##
+  ## X = [L;PP;LL] where
+  ##  L = 6x1 column vector (a line)
+  ##  PP = n 4x1 column vectors (= n planes)
+  ##  LL = n m_i 6x1 column vectors (~ m_i lines in plane i) and m_i+1 scalars
+  ## W = vector of weights on data
+  ##
+  ## * planes_objectivefn_data contains the needed data clustered
+  ## along the lines in LL
+  ## * planes_objectivefn_partition contains the information about m_i
+  ##
+  ## * the first element in planes_objectivefn_data is the plane at 0.
+  ##
+  global planes_objectivefn_data planes_objectivefn_partition;
+  n=length(planes_objectivefn_partition);
+  pwl=find(planes_objectivefn_partition>1);
+  pwol=setdiff(1:n,pwl);
+  nwol=length(pwol);
+  nwl=length(pwl);
+  mwl=sum(planes_objectivefn_partition(pwl));
+  ## the lead input is L a 6x1 vector
+  ## each plane requires a 4x1 vector
+  ## each plane with s lines requires 2 3x1 vectors and s+1 scalars
+  k=6+4*nwol+6*nwl+mwl+nwl;
+  l=length(X);
+  if l!=k
+    error ("fom(X): input X should have length ",k);
+  endif
+  pp=4*nwol+6;
+  L=X(1:6);
+  PP=reshape(X(7:pp),4,nwol);
+  d=pp;
+  for i=pwl'
+    c=d+1;
+    d=c+planes_objectivefn_partition(i);
+    planes_objectivefn_data{i}.scalars=X(c:d);
+    c=d+1;
+    d=c+2;
+    w=planes_objectivefn_data{i}.tdirvec=X(c:d);
+    c=d+1;
+    d=c+2;
+    v=planes_objectivefn_data{i}.dirvec=X(c:d);
+    ## N = normal to plane
+    N=planes_objectivefn_data{i}.normal=vector_product(v,w);
+    ## the first scalar determines C:
+    C=(planes_objectivefn_data{i}.scalars)(1);
+    P=planes_objectivefn_data{i}.point=N*C;
+    planes_objectivefn_data{i}.plane=[N;C];
+    PP=[PP,[N;C]];
+  endfor
+  LL=[];
+  S=powersets(PP',[columns(PP);2]);
+endfunction
+
+%!test
+%! global planes_data_directory planes_objectivefn_partition;
+%! planes_data_directory="~/svn-ecdf/goniometer-calibration/dir+/";
+%! planes=get_planes(planes_data_directory);
+%! l=6+4*1+6*2+9+2;
+%! X=(1:l)'; X=X .* X .* X; 
+%! [L,PP,LL,S,pwl,pwol] = extract_components (X);
+
+function C = fomc (X)
+  ## usage:  C = fomc (X)
+  ##
+  ## 
+  global planes_objectivefn_data planes_objectivefn_partition;
+  [L,PP,LL,S,pwl,pwol] = extract_components (X);
+  C=line_constraintfn(L);
+  for i=pwl'
+    w=planes_objectivefn_data{i}.tdirvec;
+    v=planes_objectivefn_data{i}.dirvec;
+    C=[C;unit_constraint(v);unit_constraint(w);orthogonal_constraint(v,w)];
+  endfor
+  for p=PP
+    pc=plane_constraint(p);
+    C=[C;pc];
+  endfor
+endfunction
+
+%!test
+%! global planes_data_directory planes_objectivefn_partition;
+%! planes_data_directory="~/svn-ecdf/goniometer-calibration/dir+/";
+%! planes=get_planes(planes_data_directory);
+%! l=6+4*1+6*2+9+2;
+%! X=(1:l)';
+%! fomc(X)
+
+function t = unit_constraint (x)
+  ## usage:  t = unit_constraint (x)
+  ##
+  ## 
+  t=(norm(x,2)-1)^2;
+endfunction
+
+function t = orthogonal_constraint (x,y)
+  ## usage:  t = orthogonal_constraint (x,y)
+  ##
+  ## 
+  t=[ (x'*y)^2 ];
+endfunction
+
+function C = gline_constraint (L)
+  ## usage:  C = gline_constraint (L)
+  ##
+  ## 
+  p=L(1:3,1);
+  v=L(4:6,1);
+  w=L(7:10);
+  C=[ (v' * p)^2 ; (w' * p)^2 ; (w' * v1)^2 ; (w'*w - 1)^2 ; (v'*v - 1)^2 ];
+endfunction
+				%!test
+				%! L=(1:6)';
+				%! Cexp=[ ((4:6)*(1:3)')^2 ; ((4:6)*(4:6)' - 1)^2 ];
+				%! C=gline_constraint(L);
+				%! assert(C,Cexp);
+function C = plane_constraint (P)
+  ## usage:  C = plane_constraint (P)
+  ##
+  ## 
+  v=P(1:3);
+  C=unit_constraint(v);
+endfunction
+				%!test
+				%! P=(1:4)';
+				%! Cexp=[ ((1:3)*(1:3)' - 1)^2 ];
+				%! C=plane_constraint(P);
+				%! assert(C,Cexp);
+
 
 function planes = get_planes (
 			      directory="",
@@ -125,10 +262,10 @@ function planes = get_planes (
     planes_objectivefn_partition=cellfun(@(x) length(x.lines),planes);
   endif
 endfunction
-%!test
-%! file="gtest.csv";
-%! pl_exp={read_goniometer_data_as_lines(file)};
-%! pl=get_planes("",file);
-%! assert(pl,pl_exp);
+				%!test
+				%! file="gtest.csv";
+				%! pl_exp={read_goniometer_data_as_lines(file)};
+				%! pl=get_planes("",file);
+				%! assert(pl,pl_exp);
 
-#  end of fom.m 
+##  end of fom.m 
