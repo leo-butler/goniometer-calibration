fb1acfdfa0d929500d188f5c645a15e07bc0f178
work <work@squeeeze.butler.org>
2013-05-30 15:43:21 -0400

    added tests

--- estimator_closed_form.m
+++ estimator_closed_form.m
@@ -49,7 +49,114 @@ function [l,L,P,G] = estimator_closed_form (planar_line_data_str,focal_plane_nor
   L=intersection_lines(P);
   l=estimate_line_from_lines_closed_form(L);
 endfunction
+%!shared epsilon, dist
 %!test
-%! planar_line_data_str=
+%! epsilon=1e-9;
+%! dist=@(x,y) min([norm(x-y,2);norm(x+y,2)]);
+%! focal_plane_normal=[0;0;1];
+%! planar_line_data_str{1}.z=planar_line_data_str{2}.z=[0;1];
+%! planar_line_data_str{1}.lines={[0,0,0;0,1,0],[0,0,1;0,1,1]};  #y-z plane
+%! planar_line_data_str{2}.lines={[0,0,0;1,0,0],[1,0,1;1,0,1]};  #x-z plane
+%! [l,L,P,G]=estimator_closed_form(planar_line_data_str,focal_plane_normal);
+%! lexp=[0;0;0;0;0;1]; # z-axis
+%! Lexp=lexp;
+%! Pexp=[1,0,0,0; 0,-1,0,0]'; # y-z plane and x-z plane
+%! assert(dist(l,lexp),0,epsilon);
+%! assert(dist(L,Lexp),0,epsilon);
+%! assert(dist(P,Pexp),0,epsilon);
+%! [g,~]=qr(randn(3,3)); g=g*g;
+%! timesg=@(x) cellfun(@(y) y*g',x,'UniformOutput',false);
+%! focal_plane_normal=g*focal_plane_normal;
+%! planar_line_data_str{1}.lines=timesg({[0,0,0;0,1,0],[0,0,1;0,1,1]});
+%! planar_line_data_str{2}.lines=timesg({[0,0,0;1,0,0], [1,0,1;1,0,1]});
+%! [l,L,P,G]=estimator_closed_form(planar_line_data_str,focal_plane_normal);
+%! lexp=[0;0;0;g(:,3)]; # z-axis
+%! Lexp=lexp;
+%! Pexp=[g,zeros(3,1);zeros(1,4)]*[1,0,0,0; 0,-1,0,0]';
+%! assert(dist(l,lexp),0,epsilon);
+%! assert(dist(L,Lexp),0,epsilon);
+%! assert(dist(P,Pexp)*dist(P,Pexp*diag([1,-1])),0,epsilon);
+%!test
+%! focal_plane_normal=[0;0;1];
+%! planar_line_data_str{1}.z=planar_line_data_str{2}.z=planar_line_data_str{3}.z=[0;1];
+%! planar_line_data_str{1}.lines={[0,0,0;0,1,0],[0,0,1;0,1,1]};  #y-z plane x=0
+%! planar_line_data_str{2}.lines={[0,0,0;1,0,0],[1,0,1;1,0,1]};  #x-z plane y=0
+%! planar_line_data_str{3}.lines={[0,0,0;1,-1,0],[1,-1,1;2,-2,1]};  # plane x+y=0
+%! [l,L,P,G]=estimator_closed_form(planar_line_data_str,focal_plane_normal);
+%! lexp=[0;0;0;0;0;1]; # z-axis
+%! Lexp=[zeros(5,3);-1,1,1];
+%! Pexp=[1,0,0,0; 0,-1,0,0; 1/sqrt(2),1/sqrt(2),0,0]'; # y-z plane and x-z plane
+%! assert(dist(l,lexp),0,epsilon);
+%! assert(dist(L,Lexp),0,epsilon);
+%! assert(dist(P,Pexp),0,epsilon);
+%!test
+%! focal_plane_normal=[0;0;1];
+%! planar_line_data_str{1}.z=planar_line_data_str{2}.z=planar_line_data_str{3}.z=[0;1];
+%! planar_line_data_str{1}.lines={[1,0,0;1,1,0],[1,0,1;1,1,1]};  #plane x=1
+%! planar_line_data_str{2}.lines={[0,1,0;1,1,0],[1,1,1;1,1,1]};  #plane y=1
+%! planar_line_data_str{3}.lines={[0,0,0;1,1,0],[1,1,1;2,2,1]};  # plane x-y=0
+%! [l,L,P,G]=estimator_closed_form(planar_line_data_str,focal_plane_normal);
+%! lexp=[1;1;0;0;0;-1]; # z-axis
+%! Lexp=[ones(2,3);zeros(3,3);-1,-1,1];
+%! Pexp=[1,0,0,1; 0,-1,0,-1; 1/sqrt(2),-1/sqrt(2),0,0]'; # y-z plane and x-z plane
+%! assert(dist(l,lexp),0,epsilon);
+%! assert(dist(L,Lexp),0,epsilon);
+%! assert(dist(P,Pexp),0,epsilon);
+%!test
+%! focal_plane_normal=[0;0;1];
+%! planar_line_data_str{1}.z=planar_line_data_str{2}.z=planar_line_data_str{3}.z=(0:5)';
+%! planar_line_data_str{1}.lines=cellfun(@(z) [1,0,z;1,1,z], num2cell(0:5), 'UniformOutput', false);  #plane x=1
+%! planar_line_data_str{2}.lines=cellfun(@(z) [0,1,z;2,1,z], num2cell(0:5), 'UniformOutput', false);  #plane y=1
+%! planar_line_data_str{3}.lines=cellfun(@(z) [0,0,z;3,3,z], num2cell(0:5), 'UniformOutput', false);  # plane x-y=0
+%! [l,L,P,G]=estimator_closed_form(planar_line_data_str,focal_plane_normal);
+%! lexp=[1;1;0;0;0;-1]; # z-axis
+%! Lexp=[ones(2,3);zeros(3,3);-1,-1,1];
+%! Pexp=[1,0,0,1; 0,-1,0,-1; 1/sqrt(2),-1/sqrt(2),0,0]'; # y-z plane and x-z plane
+%! assert(dist(l,lexp),0,epsilon);
+%! assert(dist(L,Lexp),0,epsilon);
+%! assert(dist(P,Pexp),0,epsilon);
+%!test
+%! focal_plane_normal=[0;0;1]; sigma=1e-1; epsilon=5*sigma;
+%! rndmat=@(m,z) m(z)+sigma*[rand(2,2),zeros(2,1)];
+%! planar_line_data_str{1}.z=planar_line_data_str{2}.z=planar_line_data_str{3}.z=(0:5)';
+%! planar_line_data_str{1}.lines=cellfun(@(z) rndmat(@(z) [1,0,z;1,1,z],z), num2cell(0:5), 'UniformOutput', false);  #plane x=1
+%! planar_line_data_str{2}.lines=cellfun(@(z) rndmat(@(z) [0,1,z;2,1,z],z), num2cell(0:5), 'UniformOutput', false);  #plane y=1
+%! planar_line_data_str{3}.lines=cellfun(@(z) rndmat(@(z) [0,0,z;3,3,z],z), num2cell(0:5), 'UniformOutput', false);  # plane x-y=0
+%! [l,L,P,G]=estimator_closed_form(planar_line_data_str,focal_plane_normal);
+%! lexp=[1;1;0;0;0;-1]; # z-axis
+%! Lexp=[ones(2,3);zeros(3,3);-1,-1,1];
+%! Pexp=[1,0,0,1; 0,-1,0,-1; 1/sqrt(2),-1/sqrt(2),0,0]'; # y-z plane and x-z plane
+%! assert(dist(l(1:3),lexp(1:3)),0,epsilon);
+%! assert(dist(l(4:6),lexp(4:6)),0,epsilon);
+%! assert(dist(L(1:3,:),Lexp(1:3,:)),0,epsilon);
+%! assert(dist(L(4:6,1),Lexp(4:6,1)),0,epsilon);
+%! assert(dist(L(4:6,2),Lexp(4:6,2)),0,epsilon);
+%! assert(dist(L(4:6,3),Lexp(4:6,3)),0,epsilon);
+%! assert(dist(P(:,1),Pexp(:,1)),0,epsilon);
+%! assert(dist(P(:,2),Pexp(:,2)),0,epsilon);
+%! assert(dist(P(:,3),Pexp(:,3)),0,epsilon);
+##
+%!xtest
+%! focal_plane_normal=[0;0;1]; sigma=1e-2; epsilon=5*sigma+1e-8;
+%! rndmat=@(m,z) m(z)+sigma*[rand(2,2),zeros(2,1)];
+%! planar_line_data_str{1}.z=planar_line_data_str{2}.z=(0:5)'; planar_line_data_str{3}.z=0;
+%! planar_line_data_str{1}.lines=cellfun(@(z) rndmat(@(z) [1,0,z;1,1,z],z), num2cell(0:5), 'UniformOutput', false);  #plane x=1
+%! planar_line_data_str{2}.lines=cellfun(@(z) rndmat(@(z) [0,1,z;2,1,z],z), num2cell(0:5), 'UniformOutput', false);  #plane y=1
+%! planar_line_data_str{3}.lines=cellfun(@(z) rndmat(@(z) [0,0,z;3,3,z],z), num2cell(1  ), 'UniformOutput', false);  # plane x-y=0
+%! [l,L,P,G]=estimator_closed_form(planar_line_data_str,focal_plane_normal)
+%! lexp=[1;1;0;0;0;-1]; # z-axis
+%! Lexp=[ones(2,3);zeros(3,3);-1,-1,1];
+%! Pexp=[1,0,0,1; 0,-1,0,-1; 1/sqrt(2),-1/sqrt(2),0,0]'; # y-z plane and x-z plane
+%! assert(dist(l(1:3),lexp(1:3)),0,epsilon);
+%! assert(dist(l(4:6),lexp(4:6)),0,epsilon);
+%! assert(dist(L(1:3,:),Lexp(1:3,:)),0,epsilon);
+%! assert(dist(L(4:6,1),Lexp(4:6,1)),0,epsilon);
+%! assert(dist(L(4:6,2),Lexp(4:6,2)),0,epsilon);
+%! assert(dist(L(4:6,3),Lexp(4:6,3)),0,epsilon);
+%! assert(dist(P(:,1),Pexp(:,1)),0,epsilon);
+%! assert(dist(P(:,2),Pexp(:,2)),0,epsilon);
+%! assert(dist(P(:,3),Pexp(:,3)),0,epsilon);
+
+
 
 #  end of estimator_closed_form.m 
