528788ef57b383c1a4b5965a19ec2b1694a8dfbc
work <work@squeeeze.butler.org>
2013-05-31 08:34:05 -0400

    added views and subplots

--- mc_scratch.m
+++ mc_scratch.m
@@ -34,15 +34,23 @@ function y = sc2 (n=20,N=500,sigma=1)
   plot3(y(4,:),y(5,:),y(6,:),'*');
 endfunction
 
-function plot_planar_line_data (pld,T=10,filename="mc_scratch.png")
-  clf("reset");
-  hold on;
+function plot_planar_line_data (pld,T=10)
+  hold;
   cellfun(@(x) cellfun(@(y) plot3(y(:,1), y(:,2), y(:,3), 'x'), x.lines, 'UniformOutput', false), pld.planar_line_data_str, 'UniformOutput', false);
   line=(pld.estimate.l)(1:3) + (-T:T).*(pld.estimate.l)(4:6); line=line';
   plot3(line(:,1),line(:,2),line(:,3),'*');
-  view(290,90-65);
-  hold off;
-  saveas(1,filename);
+  hold;
+endfunction
+
+function mplot_planar_line_data (pld,T=10,filename="mc_scratch")
+  clf("reset");
+  subplot(2,2,1);
+  plot_planar_line_data(pld,T); view(290,90-65);
+  subplot(2,2,2); plot_planar_line_data(pld,T); view(2,0);
+  subplot(2,2,3); plot_planar_line_data(pld,T); view(270.5,0);
+  subplot(2,2,4); plot_planar_line_data(pld,T); view(279.5,90-42);
+  saveas(1,strcat(filename,".png"));
+  save(strcat(filename,".dat"),"pld");
 endfunction
 
 function p = projection_onto_plane (pld)
@@ -61,6 +69,6 @@ function p = projection_onto_plane (pld)
   end
 endfunction
 
-sc=sc1(3,0.25,0,1); plot_planar_line_data(sc,5);
+sc=sc1(3,0.25,0,1); mplot_planar_line_data(sc,5);
 
 #  end of mc_scratch.m 
