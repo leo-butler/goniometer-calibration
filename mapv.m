505c3c5a8dd07beefa07c770e59e351dd00e8065
Leo Butler <nb0yjxtr+github@sdf.lonestar.org>
2013-10-10 16:24:27 -0400

    mapv - for mapping over vectors - kludge

---
+++ mapv.m
@@ -0,0 +1,33 @@
+## -*- Mode: octave; Package: OCTAVE -*-
+##
+## $Id:$
+##
+## Author: Leo Butler (l.butler@cmich.edu)
+##
+## This file is OCTAVE code (http://www.octave.org/)
+##
+## It is free software; you can redistribute it and/or modify
+## it under the terms of the GNU General Public License as published by
+## the Free Software Foundation; either version 3 of the License, or (at your
+## option) any later version.
+##
+## This software is distributed in the hope that it will be useful, but
+## WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
+## or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public
+## License for more details.
+##
+## You should have received a copy of the GNU General Public License
+## along with this file. If not, see http://www.gnu.org/licenses/.
+##
+
+function y = mapv (f,v,r=false)
+  if !r
+    r=rows(v);
+  endif
+  c=columns(v);
+  y=zeros(r,c);
+  for i=1:c
+    y(:,i)=f(v(:,i));
+  endfor
+endfunction
+#  end of mapv.m 
